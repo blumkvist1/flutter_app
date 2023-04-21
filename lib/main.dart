7808406989e539a8bf9edba669471b4b36d903f2
blumkvist1 <101320457+blumkvist1@users.noreply.github.com>
2023-04-21 20:48:49 +0300

    test dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,97 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyFirstApp());
+
+class MyFirstApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyFirstAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyFirstAppState extends State<MyFirstApp> {
+  bool _loading = false;
+  double _progressValue = 0;
+
+  @override
+  void initState() {
+    _loading = false;
+    _progressValue = 0;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-      ),
-      home: const MyHomePage(title: 'Just fuck aPP'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter+=3;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'How blyati i must to do it',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          leading: Icon(Icons.facebook),
+          title: const Text("facebook"),
+          centerTitle: true,
+        ),
+        body: bodyWidget(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
+          child: Icon(Icons.cloud_download),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Widget bodyWidget() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: _loading
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    LinearProgressIndicator(value: _progressValue),
+                    Text(
+                      '${(_progressValue * 100).round()}%',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                )
+              : Text(
+                  "Press button to download",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+        ),
       ),
     );
+  }
+
+  void _updateProgress() {
+    const oneSec = Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer t) {
+      setState(() {
+        _progressValue += 0.02;
+        if (_progressValue.toStringAsFixed(1) == '1.0') {
+          _loading = false;
+          t.cancel();
+          _progressValue = 0.0;
+          return;
+        }
+      });
+    });
   }
 }
